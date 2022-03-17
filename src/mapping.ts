160e7b4e91cd10e253bb7c62617b489dccac35c5
Allan Santiago <allan75imp@gmail.com>
2022-03-17 01:39:27 -0300

    feat: already for deploy

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,34 +1,37 @@
-import { near, BigInt } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
+import {
+  TokenIPFSPathUpdated as TokenIPFSPathUpdatedEvent,
+  Transfer as TransferEvent,
+  Token as TokenContract,
+} from "../generated/Token/Token";
 
-export function handleReceipt(
-  receiptWithOutcome: near.ReceiptWithOutcome
-): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(receiptWithOutcome.receipt.id.toHex())
+import { Token, User } from "../generated/schema";
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(receiptWithOutcome.receipt.id.toHex())
+export function handleTransfer(event: TransferEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    token.creator = event.params.to.toHexString();
+    token.tokenID = event.params.tokenId;
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    let tokenContract = TokenContract.bind(event.address);
+    token.contentURI = tokenContract.tokenURI(event.params.tokenId);
+    token.tokenIPFSPath = tokenContract.getTokenIPFSPath(event.params.tokenId);
+    token.name = tokenContract.name();
+    token.createdAtTimestamp = event.block.timestamp;
   }
+  token.owner = event.params.to.toHexString();
+  token.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let user = User.load(event.params.to.toHexString());
+  if (!user) {
+    user = new User(event.params.to.toHexString());
+    user.save();
+  }
+}
 
-  // Entity fields can be set based on receipt information
-  entity.block = receiptWithOutcome.block.header.hash
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+export function handleTokenURIUpdated(event: TokenIPFSPathUpdatedEvent): void {
+  let token = Token.load(event.params.tokenId.toString());
+  if (!token) return;
+  token.tokenIPFSPath = event.params.tokenIPFSPath;
+  token.save();
 }
